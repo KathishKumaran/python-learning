cfe3bc70f8e644db5b6d59227912fe72e473bab4
kathish <kathish@yavar.in>
2023-04-03 09:33:29 +0530

    python learning

---
+++ elif.py
@@ -0,0 +1,7 @@
+a=42
+if a>50:
+  print('greater than 50')
+elif a<20:
+  print('less than 20')
+else:
+  print('between 20 and 50')
---
+++ if-statement.py
@@ -0,0 +1,8 @@
+a=42
+if a>50:
+  print("a is greater than 50")
+else:
+  if a<20:
+    print('less than 20')
+  else:
+    print('a is between 20 and 50')
